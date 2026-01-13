7c00103613423efd899a1fa0ff20091184e514fe
uk <ukiiing00@gmail.com>
2026-01-13 10:44:46 +0900

    Implement order retrieval and editing features in Orders module. Add queries for fetching multiple orders and a single order based on user roles. Enhance order editing logic with role-based access control and status validation. Update Order entity to include new order status. Improve error handling for order operations.

--- backend/src/orders/orders.resolver.ts
+++ backend/src/orders/orders.resolver.ts
@@ -1,10 +1,13 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Order } from './entities/order.entity';
 import { OrderService } from './orders.service';
 import { CreateOrderInput, CreateOrderOutput } from './dto/create-order.dto';
 import { Role } from 'src/auth/role.decorator';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { User } from 'src/users/entities/user.entity';
+import { GetOrdersInput, GetOrdersOutput } from './dto/get-orders.dto';
+import { GetOrderInput, GetOrderOutput } from './dto/get-order.dto';
+import { EditOrderInput, EditOrderOutput } from './dto/edit-order.dto';
 
 @Resolver(() => Order)
 export class OrderResolver {
@@ -17,5 +20,32 @@ export class OrderResolver {
     @Args('input') createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     return this.orderService.createOrder(customer, createOrderInput);
+  }
+
+  @Query(() => GetOrdersOutput)
+  @Role(['Any'])
+  getOrders(
+    @AuthUser() user: User,
+    @Args('input') getOrderInput: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    return this.orderService.getOrders(user, getOrderInput);
+  }
+
+  @Query(() => GetOrderOutput)
+  @Role(['Any'])
+  getOrder(
+    @AuthUser() user: User,
+    @Args('input') getOrderInput: GetOrderInput,
+  ): Promise<GetOrderOutput> {
+    return this.orderService.getOrder(user, getOrderInput);
+  }
+
+  @Mutation(() => EditOrderOutput)
+  @Role(['Any'])
+  async editOrder(
+    @AuthUser() user: User,
+    @Args('input') editOrderInput: EditOrderInput,
+  ): Promise<EditOrderOutput> {
+    return this.orderService.editOrder(user, editOrderInput);
   }
 }
--- backend/src/orders/orders.service.ts
+++ backend/src/orders/orders.service.ts
@@ -1,12 +1,15 @@
 import { Repository } from 'typeorm';
-import { Order } from './entities/order.entity';
+import { Order, OrderStatus } from './entities/order.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dto/create-order.dto';
-import { User } from 'src/users/entities/user.entity';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Dish } from 'src/restaurants/entities/dish.entity';
+import { GetOrdersInput, GetOrdersOutput } from './dto/get-orders.dto';
+import { GetOrderInput, GetOrderOutput } from './dto/get-order.dto';
+import { EditOrderInput, EditOrderOutput } from './dto/edit-order.dto';
 
 @Injectable()
 export class OrderService {
@@ -78,6 +81,117 @@ export class OrderService {
       return { ok: true };
     } catch {
       return { ok: false, error: 'Could not create order.' };
+    }
+  }
+
+  async getOrders(
+    user: User,
+    { status }: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    try {
+      let orders: Order[];
+      if (user.role === UserRole.Client) {
+        orders = await this.orders.find({
+          where: { customer: user, ...(status && { status }) },
+        });
+      } else if (user.role === UserRole.Delivery) {
+        orders = await this.orders.find({
+          where: { driver: user, ...(status && { status }) },
+        });
+      } else if (user.role === UserRole.Owner) {
+        const restaurants = await this.restaurants.find({
+          where: { owner: user },
+          relations: ['orders'],
+        });
+        orders = restaurants.map((restaurant) => restaurant.orders).flat(1);
+        if (status) {
+          orders = orders.filter((order) => order.status === status);
+        }
+      } else {
+        return { ok: false, error: 'User not found.' };
+      }
+      return { ok: true, orders };
+    } catch {
+      return { ok: false, error: 'Could not get orders.' };
+    }
+  }
+
+  canSeeOrder(user: User, order: Order): boolean {
+    if (user.role === UserRole.Owner && order.restaurant?.ownerId !== user.id) {
+      return false;
+    }
+    if (user.role === UserRole.Delivery && order.driver !== user) {
+      return false;
+    }
+    if (user.role === UserRole.Client && order.customer !== user) {
+      return false;
+    }
+    return true;
+  }
+
+  async getOrder(
+    user: User,
+    { id: orderId }: GetOrderInput,
+  ): Promise<GetOrderOutput> {
+    try {
+      const order = await this.orders.findOne({
+        where: { id: orderId },
+        relations: ['restaurant'],
+      });
+
+      if (!order) {
+        return { ok: false, error: 'Order not found.' };
+      }
+      const canSee = this.canSeeOrder(user, order);
+      if (!canSee) {
+        return { ok: false, error: 'You are not allowed to see this order.' };
+      }
+      return { ok: true, order };
+    } catch {
+      return { ok: false, error: 'Could not get order.' };
+    }
+  }
+
+  async editOrder(
+    user: User,
+    { id: orderId, status }: EditOrderInput,
+  ): Promise<EditOrderOutput> {
+    try {
+      const order = await this.orders.findOne({
+        where: { id: orderId },
+        relations: ['restaurant'],
+      });
+      if (!order) {
+        return { ok: false, error: 'Order not found.' };
+      }
+      const canSee = this.canSeeOrder(user, order);
+      if (!canSee) {
+        return { ok: false, error: 'You are not allowed to edit this order.' };
+      }
+      let canEdit = true;
+      if (user.role === UserRole.Client) {
+        canEdit = false;
+      }
+      if (user.role === UserRole.Owner) {
+        if (status !== OrderStatus.Cooked && status !== OrderStatus.Cooking) {
+          canEdit = false;
+        }
+      }
+      if (user.role === UserRole.Delivery) {
+        if (
+          status !== OrderStatus.PickedUp &&
+          status != OrderStatus.Delivered
+        ) {
+          canEdit = false;
+        }
+      }
+      if (!canEdit) {
+        return { ok: false, error: 'You are not allowed to edit this order.' };
+      }
+      await this.orders.save({ id: orderId, status });
+      return { ok: true };
+    } catch {
+      return { ok: false, error: 'Could not edit order.' };
     }
   }
 }
